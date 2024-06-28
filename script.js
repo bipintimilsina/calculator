c66c341d8afeab14485370638936d49b87165e14
Bipin Timilsina <timilsinabipin20@gmail.com>
2024-06-28 15:33:03 +0545

    Added two div display system for calculator

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let numbers = document.querySelectorAll(".number");
+  numbers;
 
+  console.log(numbers);
+
+  let clear = document.querySelectorAll(".operators");
+  let operators = document.querySelector("#clear");
+  let allClear = document.querySelector("#allclear");
+  let decimal = document.querySelector("#decimal");
+  let equal = document.querySelector("#equal");
+});
